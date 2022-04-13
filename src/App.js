f537e98ebc424ec6fa769766a7ff55751c8e7c31
Molly <mojonesycreative@gmail.com>
2022-04-13 18:11:02 -0400

    Added Header, Footer, WelcomeBack .jsx files to practice components & props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import Header from "./Header";
+import WelcomeBack from "./WelcomeBack";
+import Footer from "./Footer";
+
+// function App() {
+//   const name = "Molly";
+//   const url = "https://www.thinkful.com/";
+//   const linkText = "Thinkful";
+//   const element = (
+//     <>
+//       <h1>Hello World!</h1>
+//       <h2>Have a good day.</h2>
+//       <hr />
+//         <p className='paragraph'>
+//           Hello {name}! Please visit <a href = {url}>{linkText}</a>.
+//         </p>
+//       <img src="./public/favicon.ico" alt="React Logo" width="40px" />
+//     </>
+//   );
+
+//   return element;
+// }
+
+
+
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+        <WelcomeBack name="Molly" adjective="anxious" country="Norway"/>
+        <WelcomeBack />
+      <Footer />
+    </>
+  );
 }
+
+
 
 export default App;
