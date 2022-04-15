55f61a2edfd86e6b6928df5712cd8656b1cf9663
Molly <mojonesycreative@gmail.com>
2022-04-14 20:33:44 -0400

    practice styling and added google font link

--- src/WelcomeBack.jsx
+++ src/WelcomeBack.jsx
@@ -1,8 +1,25 @@
-import "./WelcomeBack.css";
+// import "./WelcomeBack.css";
 
 
 function WelcomeBack({ name = "person", adjective = "nice", country = "Peru"}) {
-    return <p>Welcome back {adjective} {name} from {country} :)</p>;
+  const pStyle = {
+    color: "black",
+    fontFamily: "Zen Dots, cursive",  //cursive is the fallback font
+  };
+  const userName = {
+    fontFamily: "cursive",
+    padding: "2px 4px",
+    marginRight: "2px",
+    backgroundColor: "darkblue",
+    color: "#fff",
+    borderRadius: "4px",
+  };
+
+    return (
+      <p style={pStyle}>
+        Welcome back {adjective} <span style={userName}>{name}</span> from {country} :)
+      </p>
+    );
   }
 
 export default WelcomeBack;
